0e3b11ffa1ce54c836f1429dd1d2a6d8ca291124
Avi-000-Avi <atoppo267@gmail.com>
2020-12-17 01:22:40 +0530

    getting rid of bugs, still line_items undefined

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {Products, Navbar} from './components';
+import {Products, Navbar,Cart} from './components';
 import {commerce} from './lib/commerce';
 
 //import Products from './components/Products/Products';
@@ -9,35 +9,37 @@ import {commerce} from './lib/commerce';
 
 const App = () => {
     const [products , setProducts] = useState([]);
-    const [cart , setCart] = useState({});
-    //Fetch the repsonse from  await so we have to await somehing and that something is going to be a specific api call to this commerce instance 
-    //This will return a promise so we to await to see what is inside of the promise
+    const [cart, setCart] = useState({});
     const fetchProducts = async () => {
         const {data} = await commerce.products.list();
     
         setProducts(data);
     }
 
+    
     const fetchCart = async () => {
         setCart(await commerce.cart.retrieve());
-    }
+    };
 
-    const handleAddTocart = async (productId,quantity) => {
-        const item = await commerce.cart.add(productId,quantity);
-    
+    const handleAddToCart = async (productId, quantity) => {
+        const item = await commerce.cart.add(productId, quantity);
+
         setCart(item.cart);
-    }
+    };
 
     useEffect(() => {
         fetchProducts();
         fetchCart();
     },[]);
-
+ 
+        console.log(cart);
+  
 
     return (
         <div>
-            <Navbar/>
-            <Products products ={products} onAddToCart = {handleAddTocart}/>
+            <Navbar totalItems = {cart.total_items}/>
+           {/*<Products products={products} onAddToCart={handleAddToCart}/>*/}
+            <Cart cart ={cart}/>
         </div>
     )
 }
