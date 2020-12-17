0e46aa99c2d1513d85edac0ecb2ece5d07bb45eb
Avi-000-Avi <atoppo267@gmail.com>
2020-12-17 17:18:54 +0530

    CartItems done and started working on Routers

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,21 +2,24 @@ import React from 'react';
 import {Container, Typography, Button, Grid} from '@material-ui/core';
 import useStyles from './styles';
 import CartItem from './CartItem/CartItem';
+import {Link} from 'react-router-dom';
 
-const Cart = ({cart}) => {
+const Cart = ({cart ,handleUpdateCartQty,handleRemoveFromCart ,handleEmptyCart}) => {
     const classes = useStyles();
 
     const EmptyCart = () => (
-        <Typography variant="subtitle1">You have no items in your shopping cart, start adding some</Typography>
+        <Typography variant="subtitle1">You have no items in your shopping cart,
+            <Link to="/" className={classes.link}>start adding some
+            </Link>
+        </Typography>
     );
     console.log(cart.line_items);
     const FilledCart = () => (
         <>
-            
             <Grid container spacing ={3}>
                 {cart.line_items.map((item) => (
                     <Grid item xs ={12} sm= {4} key ={item.id}>        
-                        <CartItem item ={item}/>  
+                        <CartItem item ={item} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart}/>  
                     </Grid>
                 ))}
             </Grid>
@@ -25,7 +28,7 @@ const Cart = ({cart}) => {
                          Subtotal: {cart.subtotal.formatted_with_symbol}
                     </Typography>
                     <div>
-                        <button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary">Empty</button>
+                        <button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" onclick ={handleEmptyCart}>Empty</button>
                         <button className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</button>
                     </div>
             </div>
